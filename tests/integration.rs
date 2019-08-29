15cdb3eb500f36b0683b431ce9dac57a1fcbbfc6
Sebastian Schmidt <isibboi@gmail.com>
2019-08-29 10:58:01 +0300

    Refactor regex tests into own module

--- tests/integration.rs
+++ tests/integration.rs
@@ -292,34 +292,6 @@ fn test_builtin_functions() {
 }
 
 #[test]
-#[cfg(feature = "regex_support")]
-fn test_regex_functions() {
-    assert_eq!(
-        eval("str::regex_matches(\"foobar\", \"[ob]{3}\")"),
-        Ok(Value::Boolean(true))
-    );
-    assert_eq!(
-        eval("str::regex_matches(\"gazonk\", \"[ob]{3}\")"),
-        Ok(Value::Boolean(false))
-    );
-    match eval("str::regex_matches(\"foo\", \"[\")") {
-        Err(EvalexprError::InvalidRegex { regex, message }) => {
-            assert_eq!(regex, "[");
-            assert!(message.contains("unclosed character class"));
-        },
-        v => panic!(v),
-    };
-    assert_eq!(
-        eval("str::regex_replace(\"foobar\", \".*?(o+)\", \"b$1\")"),
-        Ok(Value::String("boobar".to_owned()))
-    );
-    assert_eq!(
-        eval("str::regex_replace(\"foobar\", \".*?(i+)\", \"b$1\")"),
-        Ok(Value::String("foobar".to_owned()))
-    );
-}
-
-#[test]
 fn test_errors() {
     assert_eq!(
         eval("-true"),
---
+++ tests/regex.rs
@@ -0,0 +1,30 @@
+#![cfg(feature = "regex_support")]
+
+use evalexpr::*;
+
+#[test]
+fn test_regex_functions() {
+    assert_eq!(
+        eval("str::regex_matches(\"foobar\", \"[ob]{3}\")"),
+        Ok(Value::Boolean(true))
+    );
+    assert_eq!(
+        eval("str::regex_matches(\"gazonk\", \"[ob]{3}\")"),
+        Ok(Value::Boolean(false))
+    );
+    match eval("str::regex_matches(\"foo\", \"[\")") {
+        Err(EvalexprError::InvalidRegex { regex, message }) => {
+            assert_eq!(regex, "[");
+            assert!(message.contains("unclosed character class"));
+        },
+        v => panic!(v),
+    };
+    assert_eq!(
+        eval("str::regex_replace(\"foobar\", \".*?(o+)\", \"b$1\")"),
+        Ok(Value::String("boobar".to_owned()))
+    );
+    assert_eq!(
+        eval("str::regex_replace(\"foobar\", \".*?(i+)\", \"b$1\")"),
+        Ok(Value::String("foobar".to_owned()))
+    );
+}
