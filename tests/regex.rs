84b5057b8ea435d968b1cb244a44b80c0e093f30
Sebastian Schmidt <isibboi@gmail.com>
2021-06-02 18:33:27 +0300

    Fix lint in tests folder.

--- tests/regex.rs
+++ tests/regex.rs
@@ -1,6 +1,7 @@
 #![cfg(feature = "regex_support")]
 
 use evalexpr::*;
+use std::panic::panic_any;
 
 #[test]
 fn test_regex_functions() {
@@ -17,7 +18,7 @@ fn test_regex_functions() {
             assert_eq!(regex, "[");
             assert!(message.contains("unclosed character class"));
         },
-        v => panic!(v),
+        v => panic_any!(v),
     };
     assert_eq!(
         eval("str::regex_replace(\"foobar\", \".*?(o+)\", \"b$1\")"),
