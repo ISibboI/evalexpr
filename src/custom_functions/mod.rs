ce2c51070c9f9e347b0eccc7357a3ac8eb81bc5b
gbemiga-viewserver <98267419+gbemiga-viewserver@users.noreply.github.com>
2024-02-24 15:38:39 +0000

    Bump version

--- src/custom_functions/mod.rs
+++ src/custom_functions/mod.rs
@@ -2,3 +2,10 @@ pub mod triangular_moving_average;
 pub mod is_null;
 pub mod simple_moving_average;
 pub mod simple_cumulative_sum;
+
+
+// Re-export the functions to the root of the crate
+pub use triangular_moving_average::triangular_moving_average;
+pub use is_null::is_null;
+pub use simple_moving_average::simple_moving_average;
+pub use simple_cumulative_sum::simple_cumulative_sum;
