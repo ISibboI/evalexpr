75028f2e3898d66da265690cc6558247ff565189
gbemiga-viewserver <98267419+gbemiga-viewserver@users.noreply.github.com>
2024-02-27 14:59:30 +0000

    Check for empty values with the arithmetic

--- src/custom_functions/is_null.rs
+++ src/custom_functions/is_null.rs
@@ -6,6 +6,16 @@ pub fn is_null(value: &Value) -> &Value {
         _ => value,
     }
 }
+
+pub fn starts_with(message: &Value, prefix: &Value) -> Value {
+    if let (Value::String(message), Value::String(prefix)) = (message, prefix) {
+        if message.starts_with(prefix) {
+            return Value::Boolean(true);
+        }
+    }
+    return Value::Boolean(false);
+}
+
 pub fn max<'a>(value1: &'a Value, value2: &'a Value) -> &'a Value {
     if value1 > value2 {
         value1
