d2b7a84610028532758ef984f597f04e129a6660
Gbemiga Meduoye <gbemiga@viewserver.com>
2024-04-05 11:03:52 +0100

    seg

--- src/custom_functions/mod.rs
+++ src/custom_functions/mod.rs
@@ -3,13 +3,14 @@ pub mod expression_functions;
 pub mod simple_moving_average;
 pub mod simple_cumulative_sum;
 pub mod back;
-mod basher_trade_model;
-mod compiled_transpose_calcuation_template;
+pub mod basher_trade_model;
+pub mod compiled_transpose_calcuation_template;
 
 
 // Re-export the functions to the root of the crate
 pub use triangular_moving_average::triangular_moving_average;
 pub use expression_functions::*;
 pub use back::*;
+pub use compiled_transpose_calcuation_template::*;
 pub use simple_moving_average::simple_moving_average;
 pub use simple_cumulative_sum::simple_cumulative_sum;
