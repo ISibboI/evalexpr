0417abff73fc518a3a87a930717f2d0e335c7eea
Gbemiga Meduoye <gbemiga@viewserver.com>
2024-04-08 14:40:56 +0100

    Added trade model

--- src/custom_functions/mod.rs
+++ src/custom_functions/mod.rs
@@ -7,6 +7,7 @@ pub mod basher_trade_model;
 pub mod compiled_transpose_calcuation_template;
 
 
+use std::fmt::Display;
 // Re-export the functions to the root of the crate
 pub use triangular_moving_average::triangular_moving_average;
 pub use expression_functions::*;
@@ -17,7 +18,19 @@ pub use simple_cumulative_sum::simple_cumulative_sum;
 use crate::Value;
 
 pub fn generate_column_name(field: &str, p1: &Value) -> String {
-    format!("{}_{}", field, get_string(p1))
+    format!("{}_{}", field.to_string(), sanitize_with_char(&get_string(p1), 'x'))
+}
+
+pub fn sanitize_with_char<T: Display>(value: &T, ch: char) -> String {
+    let mut sanitized = String::new();
+    for c in format!("{}", value).chars() {
+        if c.is_ascii_alphanumeric() {
+            sanitized.push(c);
+        } else {
+            sanitized.push(ch);
+        }
+    }
+    sanitized.replace(&format!("{ch}{ch}"), &format!("{ch}"))
 }
 
 pub fn get_string(value: &Value) -> String {
