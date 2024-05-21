707b4682d72aeb70cf2ec8c6da1b71180b0ca8ef
GmeduoyeClaymore <31854059+GmeduoyeClaymore@users.noreply.github.com>
2024-05-21 11:06:59 +0100

    Fixed adaptive trade model

--- src/templates/adaptive_stop_loss_trade_model.rs
+++ src/templates/adaptive_stop_loss_trade_model.rs
@@ -91,11 +91,11 @@ impl CompiledTransposeCalculationTemplate for AdaptiveStopLossTradeModel {
                     if current_close_value <= loop_stop_loss {
                         loop_trade_closed = true;
                         delta = Some(delta_val);
-                        reason = Some(format!("Lost {} Closing trade. Current price ({}) has fallen to or below stop loss from entry price ({}).",delta_val, current_close_value, loop_initiation_price));
+                        reason = Some(format!("Lost {} Closing trade. Current price ({}) has fallen to or below stop loss {} from entry price ({}).",delta_val, current_close_value,loop_stop_loss, loop_initiation_price));
                     } else if current_close_value >= loop_take_profit {
                         loop_trade_closed = true;
                         delta = Some(current_close_value - loop_initiation_price);
-                        reason = Some(format!("Won {} Closing trade. Current price ({}) has reached or exceeded take profit level from entry price ({}).",delta_val, current_close_value, loop_initiation_price));
+                        reason = Some(format!("Won {} Closing trade. Current price ({}) has reached or exceeded take profit level {} from entry price ({}).",delta_val, current_close_value,loop_take_profit, loop_initiation_price));
                     } else if current_close_value > loop_break_even {
                         stop_loss = Some(loop_initiation_price + self.break_even_threshold);
                     }
