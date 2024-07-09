c0ccd5951b35125ea7f34d07a4c41d32102c1b98
GmeduoyeClaymore <31854059+GmeduoyeClaymore@users.noreply.github.com>
2024-07-09 13:15:18 +0100

    Fixed delta

--- src/templates/compiled_transpose_calcuation_template.rs
+++ src/templates/compiled_transpose_calcuation_template.rs
@@ -1,5 +1,8 @@
+use std::any::Any;
 use std::collections::HashMap;
 use std::fmt::{Debug, Display};
+use std::panic;
+use std::panic::AssertUnwindSafe;
 use thin_trait_object::thin_trait_object;
 use crate::{BoxedOperatorRowTrait, Error, Value, ValueType};
 use crate::Error::CustomError;
@@ -10,6 +13,19 @@ pub trait CompiledTransposeCalculationTemplate : Send {
     fn schema(&self) -> HashMap<String,ValueType>;
     fn dependencies(&self) -> Vec<String>;
     fn commit_row(&self, row: &mut BoxedOperatorRowTrait, ordered_transpose_values: &[Value], cycle_epoch: usize) -> Result<(), Error>;
+    fn commit_row_wrapped(&self, row: &mut BoxedOperatorRowTrait, ordered_transpose_values: &[Value], cycle_epoch: usize) -> Result<(), Error>{
+        panic::catch_unwind(AssertUnwindSafe(|| self.commit_row(row, ordered_transpose_values, cycle_epoch))).map_err(|err| Error::CustomError(format!("{}", extract_message_from_any_error(err))))?
+    }
+}
+
+pub fn extract_message_from_any_error(err: Box<dyn Any+ Send>) -> String {
+    if let Some(s) = err.downcast_ref::<&str>() {
+        format!("Caught a panic with a string: {}", s)
+    } else if let Some(s) = err.downcast_ref::<String>() {
+        format!("Caught a panic with a string: {}", s)
+    } else {
+        format!("Caught a panic with an unknown type.")
+    }
 }
 
 pub fn context<C,T>(sself : Option<T>, context: C) -> Result<T, Error>
