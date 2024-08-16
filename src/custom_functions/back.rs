c7af822466cac2b7f658d46daca1260a0f7fe910
Gbemiga Meduoye <gbemiga@viewserver.com>
2024-08-16 12:10:23 +0100

    Another commit

--- src/custom_functions/back.rs
+++ src/custom_functions/back.rs
@@ -2,7 +2,7 @@ use crate::{Error, Value};
 
 pub fn back(row: &[Value], columns: &[usize]) -> Result<Value, Error> {
     if columns.is_empty() {
-        return Ok(Value::Empty);
+        return Ok(Value::Float(100f64));
     }
     Ok(row[columns[0]].clone())
 }
