4391edb045099874cf550f05fd2e10bbac44ae56
Gbemiga Meduoye <gbemiga@viewserver.com>
2024-08-16 10:28:20 +0100

    Another commit

--- src/custom_functions/expression_functions.rs
+++ src/custom_functions/expression_functions.rs
@@ -36,6 +36,33 @@ pub fn clip_value_to_range<T: Into<Value>, S: Into<Value>>(value: T, constant: S
     Ok(Value::Float(adjusted_value))
 }
 
+pub fn fallback_with_range_clipping<P: Into<Value>, L: Into<Value>, C: Into<Value>, D: Into<Value>>(
+    p_emin: P,
+    rel_score_long_temp_ps: L,
+    rel_score_long_temp: L,
+    range_to_clip: C,
+    empty_value: D,
+) -> Result<Value, Error> {
+    let p_emin: Value = p_emin.into();
+    let rel_score_long_temp_ps: Value = rel_score_long_temp_ps.into();
+    let rel_score_long_temp: Value = rel_score_long_temp.into();
+    let constant: Value = range_to_clip.into();
+
+    if p_emin.as_float_or_none()?.unwrap_or(0.0) <= 0.0 {
+        if !rel_score_long_temp_ps.is_empty() {
+            clip_value_to_range(rel_score_long_temp_ps, constant)
+        } else {
+            Ok(empty_value.into())
+        }
+    } else {
+        if !rel_score_long_temp.is_empty() {
+            clip_value_to_range(rel_score_long_temp, constant)
+        } else {
+            Ok(Value::Float(0.0)) // Assuming a default return value
+        }
+    }
+}
+
 
 
 pub fn abs<T: TryInto<Value>>(value: T) -> Result<Value, Error>
