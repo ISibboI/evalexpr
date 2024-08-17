95b510f9717741f1ea082dcbf68857550030741d
Gbemiga Meduoye <gbemiga@viewserver.com>
2024-08-17 16:12:49 +0100

    bucket indexes now start at 1

--- src/value/mod.rs
+++ src/value/mod.rs
@@ -89,10 +89,10 @@ impl PartialOrd for Value {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         match (self, other) {
             (Value::String(a), Value::String(b)) => a.partial_cmp(b),
-            (Value::Float(a), Value::Float(b)) => a.partial_cmp(b),
+            (Value::Float(a), Value::Float(b)) => OrderedFloat::from(*a as FloatType).partial_cmp(&OrderedFloat::from(*b as FloatType)),
             (Value::Int(a), Value::Int(b)) => a.partial_cmp(b),
-            (Value::Float(a), Value::Int(b)) => OrderedFloat::from(*a).partial_cmp(&OrderedFloat::from((*b as FloatType))),
-            (Value::Int(a), Value::Float(b)) => (*a as FloatType).partial_cmp(b),
+            (Value::Float(a), Value::Int(b)) => OrderedFloat::from(*a as FloatType).partial_cmp(&OrderedFloat::from((*b as FloatType))),
+            (Value::Int(a), Value::Float(b)) => (OrderedFloat::from(*a as FloatType)).partial_cmp(&OrderedFloat::from(*b as FloatType)),
             (Value::Boolean(a), Value::Boolean(b)) => a.partial_cmp(b),
             // For simplicity, Tuple and Empty comparisons are not implemented
             // Implementing tuple comparison would require comparing each element of the tuple, which is beyond this simple example
