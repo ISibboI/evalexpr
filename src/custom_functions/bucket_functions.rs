63a32c9f79cb2d15a0ae8cb7e34b2b0ffe5681ae
Gbemiga Meduoye <gbemiga@viewserver.com>
2024-08-28 09:52:36 +0100

    new generic type for each bucket type param

--- src/custom_functions/bucket_functions.rs
+++ src/custom_functions/bucket_functions.rs
@@ -5,40 +5,38 @@ use crate::Error::CustomError;
 macro_rules! generate_bucket_functions {
     ($($name:ident($($stop:ident),+)),+) => {
         $(
-            pub fn $name<T, E>(value_to_bucket: T, $($stop: E),+) -> Result<Value, Error>
-            where
-                T: std::convert::TryInto<Value>,
-                E: std::convert::TryInto<Value>,
-                <T as std::convert::TryInto<Value>>::Error: core::fmt::Debug,
-                <E as std::convert::TryInto<Value>>::Error: core::fmt::Debug
-            {
-                let value_to_bucket: Value = value_to_bucket.try_into().map_err(|_| Error::CustomError("Failed to convert value_to_bucket".to_string()))?;
-                let stops = vec![$($stop.try_into().map_err(|_| Error::CustomError("Failed to convert stop".to_string()))?),+];
-
-                // Ensure stops are in ascending order
-                for i in 0..stops.len() - 1 {
-                    if stops[i] > stops[i + 1] {
-                        return Err(Error::CustomError("Stops must be in ascending order".to_string()));
-                    }
-                }
-
-                // Determine which bucket the value belongs to
-                for (i, stop) in stops.iter().enumerate() {
-                    if value_to_bucket <= *stop {
-                        return Ok(Value::Int(i as IntType));
-                    }
-                }
-
-                Ok(Value::Int(stops.len() as IntType))
-            }
-
             paste::paste! {
-                pub fn [<$name _desc>] <T, E>(value_to_bucket: T, $($stop: E),+) -> Result<Value, Error>
+                pub fn $name<T, $([<T $stop>]),+>(value_to_bucket: T, $($stop: [<T $stop>]),+) -> Result<Value, Error>
                 where
                     T: std::convert::TryInto<Value>,
-                    E: std::convert::TryInto<Value>,
                     <T as std::convert::TryInto<Value>>::Error: core::fmt::Debug,
-                    <E as std::convert::TryInto<Value>>::Error: core::fmt::Debug
+                    $([<T $stop>]: std::convert::TryInto<Value>, <[<T $stop>] as std::convert::TryInto<Value>>::Error: core::fmt::Debug),+
+                {
+                    let value_to_bucket: Value = value_to_bucket.try_into().map_err(|_| Error::CustomError("Failed to convert value_to_bucket".to_string()))?;
+                    let stops = vec![$($stop.try_into().map_err(|_| Error::CustomError("Failed to convert stop".to_string()))?),+];
+
+                    // Ensure stops are in ascending order
+                    for i in 0..stops.len() - 1 {
+                        if stops[i] > stops[i + 1] {
+                            return Err(Error::CustomError("Stops must be in ascending order".to_string()));
+                        }
+                    }
+
+                    // Determine which bucket the value belongs to
+                    for (i, stop) in stops.iter().enumerate() {
+                        if value_to_bucket <= *stop {
+                            return Ok(Value::Int(i as IntType));
+                        }
+                    }
+
+                    Ok(Value::Int(stops.len() as IntType))
+                }
+
+                pub fn [<$name _desc>] <T, $([<T $stop>]),+>(value_to_bucket: T, $($stop: [<T $stop>]),+) -> Result<Value, Error>
+                where
+                    T: std::convert::TryInto<Value>,
+                    <T as std::convert::TryInto<Value>>::Error: core::fmt::Debug,
+                    $([<T $stop>]: std::convert::TryInto<Value>, <[<T $stop>] as std::convert::TryInto<Value>>::Error: core::fmt::Debug),+
                 {
                     let value_to_bucket: Value = value_to_bucket.try_into().map_err(|_| Error::CustomError("Failed to convert value_to_bucket".to_string()))?;
                     let stops = vec![$($stop.try_into().map_err(|_| Error::CustomError("Failed to convert stop".to_string()))?),+];
@@ -128,6 +126,41 @@ mod tests {
         assert_eq!(bucket_3_desc(Value::Int(15), stop_1.clone(), stop_2.clone(), stop_3.clone()).unwrap(), Value::String("10 - 20".into()));
         assert_eq!(bucket_3_desc(Value::Int(25), stop_1.clone(), stop_2.clone(), stop_3.clone()).unwrap(), Value::String("20 - 30".into()));
         assert_eq!(bucket_3_desc(Value::Int(35), stop_1.clone(), stop_2.clone(), stop_3.clone()).unwrap(), Value::String("> 30".into()));
+    }
+
+
+    #[test]
+    fn test_bucket_5_desc_with_float_stops() {
+        let stop_1 = Value::Float(10.5);
+        let stop_2 = Value::Float(20.5);
+        let stop_3 = Value::Float(30.5);
+        let stop_4 = Value::Float(40.5);
+        let stop_5 = Value::Float(50.5);
+
+        assert_eq!(
+            bucket_5_desc(Value::Int(5), stop_1.clone(), stop_2.clone(), stop_3.clone(), stop_4.clone(), stop_5.clone()).unwrap(),
+            Value::String("<= 10.5".into())
+        );
+        assert_eq!(
+            bucket_5_desc(Value::Int(15), stop_1.clone(), stop_2.clone(), stop_3.clone(), stop_4.clone(), stop_5.clone()).unwrap(),
+            Value::String("10.5 - 20.5".into())
+        );
+        assert_eq!(
+            bucket_5_desc(Value::Int(25), stop_1.clone(), stop_2.clone(), stop_3.clone(), stop_4.clone(), stop_5.clone()).unwrap(),
+            Value::String("20.5 - 30.5".into())
+        );
+        assert_eq!(
+            bucket_5_desc(Value::Int(35), stop_1.clone(), stop_2.clone(), stop_3.clone(), stop_4.clone(), stop_5.clone()).unwrap(),
+            Value::String("30.5 - 40.5".into())
+        );
+        assert_eq!(
+            bucket_5_desc(Value::Int(45), stop_1.clone(), stop_2.clone(), stop_3.clone(), stop_4.clone(), stop_5.clone()).unwrap(),
+            Value::String("40.5 - 50.5".into())
+        );
+        assert_eq!(
+            bucket_5_desc(Value::Int(55), stop_1.clone(), stop_2.clone(), stop_3.clone(), stop_4.clone(), stop_5.clone()).unwrap(),
+            Value::String("> 50.5".into())
+        );
     }
 
     #[test]
