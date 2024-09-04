de88812ebf808eb63f8ae09a415ea40563f2c768
Gbemiga Meduoye <gbemiga@viewserver.com>
2024-09-04 11:13:21 +0100

    Added or and and functions

--- src/custom_functions/back.rs
+++ src/custom_functions/back.rs
@@ -2,7 +2,7 @@ use crate::{BoxedOperatorRowTrait, Error, OperatorRowTrait, Value};
 
 pub fn back(row: &BoxedOperatorRowTrait, columns: &[usize]) -> Result<Value, Error> {
     if columns.is_empty() {
-        return Ok(Value::Float(100f64));
+        return Ok(Value::Empty);
     }
     Ok(row.get_value_for_column(columns[0])?)
 }
