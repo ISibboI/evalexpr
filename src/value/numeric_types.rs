b06ce59c3423b95fa8ca87c932fe3945396b2bc1
Sebastian Schmidt <isibboi@gmail.com>
2024-12-27 10:17:18 +0200

    Fix docs link.

--- src/value/numeric_types.rs
+++ src/value/numeric_types.rs
@@ -224,6 +224,6 @@ pub trait EvalexprFloat<NumericTypes: EvalexprNumericTypes<Float = Self>>:
 
     /// Generate a random float value between 0.0 and 1.0.
     ///
-    /// If the feature `rand` is not enabled, then this method always returns [`EvalexprError::RandNotEnabled`].
+    /// If the feature `rand` is not enabled, then this method always returns [`EvalexprError::RandNotEnabled`](crate::EvalexprError::RandNotEnabled).
     fn random() -> EvalexprResult<Self, NumericTypes>;
 }
